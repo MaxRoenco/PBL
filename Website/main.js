ee5ea10e6f21861c0ca0bffbd556076f817ba761
IslamAbukoush <islam.abu.koush@isa.utm.md>
2024-04-03 12:31:36 +0300

    gg

--- Website/main.js
+++ Website/main.js
@@ -9,6 +9,14 @@ let tabs = document.getElementById("tabs").children;
 Array.from(tabs).forEach(ele => {
     ele.style.display = "none";
 });
+let settingsButton = document.getElementById("settingsIcon");
+let profileButton = document.getElementById("profileIcon");
+let categoriesButton = document.getElementById("categoriesReturn");
+let lessonsButton = document.getElementById("lessonsReturn");
+settingsButton.style.display = "hidden";
+profileButton.style.display = "hidden";
+categoriesButton.style.display = "hidden";
+lessonsButton.style.display = "hidden";
 console.log("ran once")
 openTab("home");
 fetchData();
