7d964edb5aee287231d81899ed5f5249871e0b51
Roenco Maxim <maxroenco2004@gmail.com>
2024-06-12 15:53:40 +0300

    ghjkl

--- Website/JavaScript/store.js
+++ Website/JavaScript/store.js
@@ -22,3 +22,30 @@ hearts.addEventListener('click', () => {
     holsterDiamonds.style.display = 'none';
     holster.style.display = 'none';
 })
+
+
+let cursorMouse1 = document.getElementById('cursorMouse1');
+let cursorMouse2 = document.getElementById('cursorMouse2');
+let cursorMouse3 = document.getElementById('cursorMouse3');
+let cursorMouse4 = document.getElementById('cursorMouse4');
+let cursorMouse5 = document.getElementById('cursorMouse5');
+
+cursorMouse1.addEventListener('click', () => {
+    document.body.style.cursor = "url('./assets/images/mouse6.png'), auto";
+})
+
+cursorMouse2.addEventListener('click', () => {
+    document.body.style.cursor = "url('./assets/images/mouse5.png'), auto";
+})
+
+cursorMouse3.addEventListener('click', () => {
+    document.body.style.cursor = "url('./assets/images/mouse3.png'), auto";
+})
+
+cursorMouse4.addEventListener('click', () => {
+    document.body.style.cursor = "url('./assets/images/mouse4.png'), auto";
+})
+
+cursorMouse5.addEventListener('click', () => {
+    document.body.style.cursor = "auto";
+})
